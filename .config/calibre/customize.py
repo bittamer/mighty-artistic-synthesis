5e2eff13f16b1b4aca25398c708dc335a2f64049
Hubbi Nashrullah Muhammad <me@hubbinash.org>
2018-05-23 19:12:07 +0700

    .config

---
+++ .config/calibre/customize.py
@@ -0,0 +1,26 @@
+# 
+
+### Begin group: DEFAULT
+ 
+# plugins
+# Installed plugins
+plugins = cPickle.loads('\x80\x02}q\x01(X\x0e\x00\x00\x00Barnes & NobleX6\x00\x00\x00/home/hubbi/.config/calibre/plugins/Barnes & Noble.zipX\r\x00\x00\x00Manage SeriesX5\x00\x00\x00/home/hubbi/.config/calibre/plugins/Manage Series.zipX\t\x00\x00\x00GoodreadsX1\x00\x00\x00/home/hubbi/.config/calibre/plugins/Goodreads.zipu.')
+ 
+# filetype mapping
+# Mapping for filetype plugins
+filetype_mapping = cPickle.loads('\x80\x02}q\x01.')
+ 
+# plugin customization
+# Local plugin customization
+plugin_customization = cPickle.loads('\x80\x02}q\x01.')
+ 
+# disabled plugins
+# Disabled plugins
+disabled_plugins = cPickle.loads('\x80\x02c__builtin__\nset\nq\x01]\x85Rq\x02.')
+ 
+# enabled plugins
+# Enabled plugins
+enabled_plugins = cPickle.loads('\x80\x02c__builtin__\nset\nq\x01]q\x02(X\x0f\x00\x00\x00Big Book Searchq\x03X\t\x00\x00\x00Edelweissq\x04X\r\x00\x00\x00Google Imagesq\x05X\n\x00\x00\x00Amazon.comq\x06e\x85Rq\x07.')
+ 
+
+
